e5a9e3043682d49ecc2b4146af3b3f5e7a9d6484
Julian Olsen <julian.olsen2002@gmail.com>
2023-09-14 14:04:42 +0200

    Create practice.py
    Just me learning how to use matplotlib for university and data Managment

---
+++ practice.py
@@ -0,0 +1,21 @@
+
+import matplotlib as mpl
+import matplotlib.pyplot as plt
+import numpy as np
+
+
+# initialisation of line graph data
+my_array = np.array([1, 10, 5, 4, 19, 0, 2, 8])
+my_array2 = np.array([12, 4, 16, 14, 20, 9, 3, 1])
+
+# plotting the first graph
+plt.plot(my_array)
+plt.show()
+
+# plotting the second graph
+plt.plot(my_array2)
+plt.show()
+
+
+# Documentation on how to use it properly is here https://matplotlib.org/stable/tutorials/introductory/quick_start.html
+# would highly recommend using this link to manipulate and shape your graphs.
